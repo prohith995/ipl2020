0b6468acd4be1e9b5eed92b1f456ae5f87f46989
Rohith Pinnamaraju <prohith.995@gmail.com>
2020-03-08 16:47:06 -0500

    nm

--- Scripts/Modelling/initial_model_regression.py
+++ Scripts/Modelling/initial_model_regression.py
@@ -29,10 +29,13 @@ df1 = df1.drop(columns = str_cols)
 from sklearn.model_selection import train_test_split
 df_train, df_test = train_test_split(df1, test_size=0.2, random_state = 0)
 
-X_train = df_train.drop(columns = ['score'])
+df_train = df1[df1['match_id_ordered'] <= 750]
+df_test = df1[df1['match_id_ordered'] > 750]
+
+X_train = df_train.drop(columns = ['score', 'match_id_ordered'])
 y_train = df_train['score']
 
-X_test = df_test.drop(columns = ['score'])
+X_test = df_test.drop(columns = ['score', 'match_id_ordered'])
 y_test = df_test['score']
 
 import statsmodels.api as sm
@@ -55,31 +58,31 @@ metrics.r2_score(y_test, y_test_pred)
 from sklearn.ensemble import RandomForestRegressor
 from sklearn.model_selection import GridSearchCV
 
-cv_grid = {'n_estimators': [50, 100, 250],
-               'max_features': [10, 20],
-               'max_depth': [20, 40],
-               'min_samples_split': [2, 4],
-               'min_samples_leaf': [1, 3],
+cv_grid = {'n_estimators': [50, 200, 500],
+               'max_features': [20, 30, 40],
+               'max_depth': [25, 50],
+               'min_samples_split': [2, 4, 8],
+               'min_samples_leaf': [3, 5, 8],
                'bootstrap': [True]}
 
-cv_grid_2 = {'n_estimators': [100, 250, 500, 1000],
+cv_grid_2 = {'n_estimators': [100, 250, 500],
                'max_features': [25, 60, 120, 250],
-               'max_depth': [50, 75, 120, 250],
-               'min_samples_split': [2, 4, 8, 16],
-               'min_samples_leaf': [1, 3, 6, 12],
-               'bootstrap': [True, False]}
+               'max_depth': [50, 100],
+               'min_samples_split': [2, 4],
+               'min_samples_leaf': [1, 3, 5],
+               'bootstrap': [True]}
 
 rf = RandomForestRegressor()
-rf_cv = GridSearchCV(rf, cv_grid)
+rf_cv = GridSearchCV(rf, cv_grid, verbose = 10, n_jobs = -1)
 rf_cv.fit(X_train, y_train)
 rf_cv.best_params_
 
-rf_cv_2 = GridSearchCV(rf, cv_grid_2)
+rf_cv_2 = GridSearchCV(rf, cv_grid_2, verbose = 10, n_jobs = -1)
 rf_cv_2.fit(X_train, y_train)
 rf_cv_2.best_params_
 
-rf = RandomForestRegressor(n_estimators = 700, max_features= 100, max_depth= 75,
-                           min_samples_split= 2, min_samples_leaf= 1, bootstrap= True)
+rf = RandomForestRegressor(n_estimators = 500, max_features= 20, max_depth= 50,
+                           min_samples_split= 8, min_samples_leaf= 8, bootstrap= True)
 rf.fit(X_train, y_train)
 y_train_pred = rf.predict(X_train)
 y_test_pred = rf.predict(X_test)
@@ -91,3 +94,6 @@ metrics.r2_score(y_train, y_train_pred)
 metrics.r2_score(y_test, y_test_pred)    
 mean_absolute_percentage_error(y_train, y_train_pred)
 mean_absolute_percentage_error(y_test, y_test_pred)    
+
+
+
